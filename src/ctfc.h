e3292814ba2d122b5ad9a79953078bc6527877c9
supjerk <teu9345@gmail.com>
2019-02-12 23:00:01 +0900

    Second

--- src/ctfc.h
+++ src/ctfc.h
@@ -26,15 +26,22 @@ char* DNSLookUp(const char* URL) {
   return inet_ntoa(*(struct in_addr*)honestUnit->h_addr_list[0]);
 }
 
-int checkIP(const char *ip) { // IPv4 only
+int checkIP(const char *ip) { /* IPv4 only */
   return (strlen(ip)>0 && strlen(ip)<16) ? 1 : 0;
 }
 
-int request(int flag, const char* ip, char* data) { // request(GET, "https://www.google.co.kr./", NULL)
-  if (!checkIP(ip)) { puts("E: Beep! The len of IP Address error dude.") exit(-1); }
+int request(int flag, const char* URL, const char* ip, char* data) { /* request(GET, "https://www.google.co.kr./", NULL) */
+  if (!checkIP(ip)) { puts("E: Beep! The len of IP Address error dude."); exit(-1); }
 
   int fdSock,lastone;
-  char buf[MAX_LEN], *str="";
+  char buf[MAX_LEN], *str="GET / HTTP/1.1\n" \
+    "Host: ctftime.org\n" \
+    "Connection: close\n" \
+    "Pragma: no-cache\n" \
+    "Cache-Control: no-cache\n" \
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\n" \
+    "Accept-Encoding: gzip, deflate\n" \
+    "Accept-Language: en-US,en;q=0.9,de;q=0.8,ko;q=0.7\n";
   struct sockaddr_in servAddr;
 
   if ((fdSock=socket(PF_INET, SOCK_STREAM, 0)) < 0) {
@@ -49,6 +56,15 @@ int request(int flag, const char* ip, char* data) { // request(GET, "https://www
   if((connect(fdSock, (struct sockaddr *)&servAddr, sizeof(servAddr))) < 0) {
     puts("E: Error on connect() function");
     exit(-1);
+  }
+
+  switch(flag) {
+    case GET :
+//      write(fdSock,)
+      break;
+    case POST :
+      /* code */
+      break;
   }
   
   if ( (lastone=read(fdSock,buf,MAX_LEN))>0 ) {
